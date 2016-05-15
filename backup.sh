eceddaf2a16da5fe0f5941f22308116386b44b15
Chris <chris@debian.personal>
2016-05-15 12:10:58 -0400

    write output to log file

--- backup.sh
+++ backup.sh
@@ -2,17 +2,19 @@
 NOW=$(date +"%Y-%m-%d-%H%M%S")
 FILE="backup.$NOW.tar.gz"
 BACKUP_DIRECTORY="/media/chris/Seagate Expansion Drive/DebianBackups"
+LOG_DIR="$BACKUP_DIRECTORY/Logs"
 
 # $BACKUP_DIRECTORY must be in double quotes since part of it's path has spaces
 # i.e., Seagate Expansion Drive
-mkdir -p "$BACKUP_DIRECTORY" 
+mkdir -p "$BACKUP_DIRECTORY"
+mkdir -p "$LOG_DIR"
 #rsync -aitvz ./* "$BACKUP_DIRECTORY/$FILE"
 
 # https://help.ubuntu.com/community/BackupYourSystem/TAR
 # http://superuser.com/questions/438450/which-files-do-i-need-to-backup-to-keep-my-linux-user-settings
 # http://unix.stackexchange.com/questions/1067/what-directories-do-i-need-to-back-up
 # leaving out -v from tar to reduce output so backup runs faster
-tar -cpzf "$BACKUP_DIRECTORY/$FILE" /etc /home /root /usr/local /usr/share /home /var
+nohup tar -vcpzf "$BACKUP_DIRECTORY/$FILE" /etc /home /root /usr/local /usr/share /var > "$LOG_DIR/backup-$NOW.log" 2>&1 &
 
 # Extraction
 # http://stackoverflow.com/questions/13707429/decompress-gzip-file-to-specific-directory
